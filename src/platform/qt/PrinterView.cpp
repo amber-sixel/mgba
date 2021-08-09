92493abfece33e1f2d64eee30839f53a87c5154d
amber-sixel <84023792+amber-sixel@users.noreply.github.com>
2021-08-09 10:52:40 -0400

    Qt: use exposure value in PrinterView

--- src/platform/qt/PrinterView.cpp
+++ src/platform/qt/PrinterView.cpp
@@ -81,6 +81,7 @@ void PrinterView::printImage(const QImage& image, int topMargin, int bottomMargi
 	QPainter painter(&pixmap);
 	painter.drawPixmap(0, 0, m_image);
 	painter.drawImage(0, m_image.height() + (topMargin * 16), image);
+	painter.fillRect(0, m_image.height() + (topMargin * 16), image.width(), image.height(), QBrush(QColor(255, 255, 255, 127-exposure)));
 	m_image = pixmap;
 	m_ui.image->setPixmap(m_image.scaled(m_image.size() * m_ui.magnification->value()));
 	m_timer.start();
